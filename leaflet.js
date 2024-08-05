16f986c094e699b0303bfd254a55563325e513a9
Leo Medina <leomedinaforest@gmail.com>
2024-08-05 18:44:21 +0200

    files untracked added

---
+++ leaflet.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([5.1632955, -69.4146705], 6);
+// osm layer
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+osm.addTo(map);
+// marker
