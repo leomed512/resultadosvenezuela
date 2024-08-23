389b63ccda2938412aa31d8c4069116e5712992a
leomedina <leomedinaforest@gmail.com>
2024-08-24 00:39:00 +0200

    new tile for 401 error

--- leaflet.js
+++ leaflet.js
@@ -13,11 +13,10 @@ map.on('drag', function() {
 
 
 // thunderforest
-var new_tile = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
-    maxZoom: 9,
+var new_tile= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 9,
     minZoom: 6,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
 });
 new_tile.addTo(map);
 
